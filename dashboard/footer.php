bfcd597dbabf9f862554e001f811a016b8f87bc3
delorian <fjarandabarba@gmail.com>
2024-03-04 17:17:31 +0100

    boostrap_esqueleto

---
+++ dashboard/footer.php
@@ -0,0 +1,25 @@
+    </main>
+    <footer class="container-fluid bg-dark text-light text-center py-2">
+        <div class="row">
+            <div class="py-3">
+                Trabajo realizado por 
+                <br>Daniel Alejandro Molina Gálvez <br>
+                <br>Francisco Javier Aranda Barba <br>
+
+            </div>
+        </div>
+        <div class="row">
+            <div class="py-3">
+                <i class="bi-envelope py-3"></i>
+                <br />
+                ejemplo@ejemplo.com
+            </div>
+        </div>
+    </footer>
+    <!-- Bootstrap JavaScript Libraries -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+    </body>
+
+    </html>
