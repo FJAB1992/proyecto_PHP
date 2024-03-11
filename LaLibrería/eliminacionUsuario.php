d1df8b6449ca5a223515fc20d2f2fff6e0cfb6cd
delorian <fjarandabarba@gmail.com>
2024-03-11 15:38:32 +0100

    boot2

--- LaLibrería/eliminacionUsuario.php
+++ LaLibrería/eliminacionUsuario.php
@@ -1,24 +1,51 @@
-<?php
-    //Me traigo la sesion
-    session_start();
+<!DOCTYPE html>
+<html lang="en">
 
-    // Incluir el archivo de conexión
-    require_once('conexion.php');
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro en Librería</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 
-    //Recoger el codigo
-    $codigo= $_GET["codigo"];
+    <style>
+        a {
+            text-decoration: none;
+            color: white;
+        }
+    </style>
+</head>
 
-    print "<h1>Informacion de usuarios</h1>";
+<body>
+    <div class="container text-center p-3 bg-success">
+        <?php
+        //Me traigo la sesion
+        session_start();
 
-    $borrar= "DELETE FROM usuarios WHERE id_usuario=$codigo";
+        // Incluir el archivo de conexión
+        require_once('conexion.php');
 
-    $resultado= mysqli_query($conexion, $borrar);
+        //Recoger el codigo
+        $codigo = $_GET["codigo"];
 
-    if($resultado==1){
-        print "Eliminado exitosamente<br>";
-        print"<a href='dashboard.php?'>volver</a>";
-    }else{
-        print"Error al eliminar";
-        print"<a href='dashboard.php?'>volver</a>";
-    }
-?>
+        print "<h1>Informacion de usuarios</h1>";
+
+        $borrar = "DELETE FROM usuarios WHERE id_usuario=$codigo";
+
+        $resultado = mysqli_query($conexion, $borrar);
+
+        if ($resultado == 1) {
+            print "Eliminado exitosamente<br>";
+            print "<a href='dashboard.php?' class='btn btn-primary m-3'>Volver</a>";
+        } else {
+            print "Error al eliminar";
+            print "<a href='dashboard.php?' class='btn btn-primary m-3'>Volver</a>";
+        }
+        ?>
+
+    </div>
+</body>
+
+</html>
