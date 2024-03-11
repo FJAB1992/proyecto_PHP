7d44b687f9f3db786c8f678a1a84304bc4893f32
delorian <fjarandabarba@gmail.com>
2024-03-11 17:32:22 +0100

    alta_casi

--- LaLibrería/crudLibro/registrar.php
+++ LaLibrería/crudLibro/registrar.php
@@ -1,51 +1,24 @@
 <?php include "conexion.php" ?>
 
 <?php
-mysqli_select_db($conexion, "productosbd");
+mysqli_select_db($conexion, "libreria");
 
 //var_dump($_POST);
 
 //almacena lo que hay en el array asociativo
-$identificador = $_POST["identificador"];
-$nombre = $_POST["nombre"];
-$descripcion = $_POST["descripcion"];
-$precio = $_POST["precio"];
+$titulo = $_POST["titulo"];
+$autor = $_POST["autor"];
+$genero = $_POST["genero"];
+$fecha_publicacion = $_POST["fecha_publicacion"];
+$disponibilidad = $_POST["disponibilidad"];
+
 
 //Mostrará nombre,tipo,ubicación, errores, tamaño; ddel archivo
 //var_dump($_FILES['imagen'])
 
-//Habrá que crearlo anteriormente
-$directorioSubida = "imagenes/";
-$max_file_size = "5120000";
-$extensionesValidas = array("jpg", "png", "gif");
-
-if (isset($_FILES['imagen'])) {
-    $errores = 0;
-    $nombreArchivo = $_FILES['imagen']['name'];
-    $filesize = $_FILES['imagen']['size'];
-    $directorioTemp = $_FILES['imagen']['tmp_name'];
-    $arrayArchivo = pathinfo($nombreArchivo);
-    //var_dump($arrayArchivo);
-    $extension = $arrayArchivo['extension'];
-
-    if (!in_array($extension, $extensionesValidas)) {
-        echo "La extensión no es válida";
-        $errores = 1;
-    }
-
-    if ($filesize > $max_file_size) {
-        echo "La imagen debe tener un tamaño inferior.";
-        $errores = 1;
-    }
-
-    if ($errores == 0) {
-        $nombreCompleto = $directorioSubida . $nombreArchivo;
-        move_uploaded_file($directorioTemp, $nombreCompleto);
-    }
-}
 
 
-$insertar = "INSERT productos (id_producto,nombre,descripcion,precio,fotografia) VALUES ($identificador,'$nombre','$descripcion',$precio,'$nombreArchivo')";
+$insertar = "INSERT libros (titulo,autor, genero,fecha_publicacion,disponibilidad) VALUES ('$titulo','$autor','$genero','$fecha_publicacion','$disponibilidad')";
 mysqli_query($conexion, $insertar);
 header("Location:alta_ok.php");
 ?>
