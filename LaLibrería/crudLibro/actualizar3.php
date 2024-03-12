2b89d25fecc75f5e73e94dc87e9478dbd0a34c40
FJAB1992 <fjarandabarba@gmail.com>
2024-03-12 13:21:32 +0100

    actualizar_updated

--- LaLibrería/crudLibro/actualizar3.php
+++ LaLibrería/crudLibro/actualizar3.php
@@ -1,59 +1,27 @@
-<?php include "conexion.php" ?>
-
 <?php
+include "conexion.php";
 
 $idm = $_GET["idmodifica"];
-$nombreantiguo = $_GET["nombreimagen"];
 
+mysqli_select_db($conexion, "libreria");
 
-mysqli_select_db($conexion, "productosbd");
+$titulo = $_POST["titulo"];
+$autor = $_POST["autor"];
+$genero = $_POST["genero"];
+$fecha_publicacion = $_POST["fecha_publicacion"];
+$disponibilidad = isset($_POST["disponibilidad"]) ? 1 : 0; // Verifica si se ha marcado la casilla de disponibilidad
 
-//var_dump($_POST);
+// Evitar la inyección SQL usando consultas preparadas
+$query = "UPDATE libros SET titulo=?, autor=?, genero=?, fecha_publicacion=?, disponibilidad=? WHERE id_libro=?";
+$stmt = mysqli_prepare($conexion, $query);
 
-//almacena lo que hay en el array asociativo
-$identificador = $_POST["identificador"];
-$nombre = $_POST["nombre"];
-$descripcion = $_POST["descripcion"];
-$precio = $_POST["precio"];
-
-//Mostrará nombre,tipo,ubicación, errores, tamaño; ddel archivo
-//var_dump($_FILES['imagen'])
-
-//Habrá que crearlo anteriormente
-$directorioSubida = "imagenes/";
-$max_file_size = "5120000";
-$extensionesValidas = array("jpg", "png", "gif");
-
-if ($_FILES['imagen']['name'] != "") {
-    $errores = 0;
-    $nombreArchivo = $_FILES['imagen']['name'];
-    $filesize = $_FILES['imagen']['size'];
-    $directorioTemp = $_FILES['imagen']['tmp_name'];
-    $arrayArchivo = pathinfo($nombreArchivo);
-    //var_dump($arrayArchivo);
-    $extension = $arrayArchivo['extension'];
-
-    if (!in_array($extension, $extensionesValidas)) {
-        echo "La extensión no es válida";
-        $errores = 1;
-    }
-
-    if ($filesize > $max_file_size) {
-        echo "La imagen debe tener un tamaño inferior.";
-        $errores = 1;
-    }
-
-    if ($errores == 0) {
-        $nombreCompleto = $directorioSubida . $nombreArchivo;
-        move_uploaded_file($directorioTemp, $nombreCompleto);
-    }
-}
-if ($_FILES['imagen']['name'] != "") {
-    $insertar = "UPDATE productos SET id_producto = $identificador, nombre = '$nombre', descripcion = '$descripcion', precio ='$precio',fotografia = '$nombreArchivo' WHERE id_producto = '$idm'";
-} else {
-    $insertar = "UPDATE productos SET id_producto = $identificador, nombre = '$nombre', descripcion = '$descripcion', precio ='$precio',fotografia = '$nombreantiguo' WHERE id_producto = '$idm'";
+// Verificar si la preparación de la consulta fue exitosa
+if ($stmt) {
+    mysqli_stmt_bind_param($stmt, "ssssii", $titulo, $autor, $genero, $fecha_publicacion, $disponibilidad, $idm);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
 }
 
-mysqli_query($conexion, $insertar);
-header("Location:alta_ok.php");
+header("Location: alta_ok.php");
+exit();
 ?>
